3e3664c9a7ea28cd2e6aff22d614b04d4aedd3f2
Jungley Yeh <yejing@live.nc>
2023-12-13 02:27:21 +0800

    feat: 定制版

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,15 @@
 export function loadTheme(theme: string, origin: string) {
-  return new Promise(resolve => {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.setAttribute('crossorigin', 'anonymous');
+  return new Promise((resolve) => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.setAttribute("crossorigin", "anonymous");
     link.onload = resolve;
-    link.href = `/stylesheets/themes/${theme}/utterances.css`;
+    link.href = `./stylesheets/themes/${theme}/utterances.css`;
     document.head.appendChild(link);
 
-    addEventListener('message', event => {
-      if (event.origin === origin && event.data.type === 'set-theme') {
-        link.href = `/stylesheets/themes/${event.data.theme}/utterances.css`;
+    addEventListener("message", (event) => {
+      if (event.origin === origin && event.data.type === "set-theme") {
+        link.href = `./stylesheets/themes/${event.data.theme}/utterances.css`;
       }
     });
   });
